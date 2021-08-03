7f80d3d2aaf57a48125e458fd4d9541a0358e7cd
Joel Calado <joelcalado@gmail.com>
2021-08-03 21:11:03 +0100

    improved main screen UI; formatting changes
    also add a discord link for help

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:core';
 import 'package:VDO.Ninja/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'dart:math';
 import 'src/call_sample/call_sample.dart';
 import 'src/route_item.dart';
@@ -16,9 +17,8 @@ var chars = 'AaBbCcDdEeFfGgHhJjKkLMmNnoPpQqRrSsTtUuVvWwXxYyZz23456789';
 Random _rnd = Random();
 
 String getRandomString(int length) => String.fromCharCodes(Iterable.generate(
-length, (_) => chars.codeUnitAt(_rnd.nextInt(chars.length))));
-var streamID =  getRandomString(8);
-
+    length, (_) => chars.codeUnitAt(_rnd.nextInt(chars.length))));
+var streamID = getRandomString(8);
 
 class MyApp extends StatefulWidget {
   @override
@@ -35,8 +35,14 @@ class _MyAppState extends State<MyApp> {
   SharedPreferences _prefs;
 
   var _screenShare = "screen";
- 
-  
+  List<Color> colors = [
+    Color(0xFFA53E97),
+    Color(0xFF645098),
+    Color(0xFF33517E),
+    Colors.amber,
+    Colors.red,
+  ];
+
   @override
   initState() {
     super.initState();
@@ -44,16 +50,26 @@ class _MyAppState extends State<MyApp> {
     _initItems();
   }
 
-  _buildRow(context, item) {
+  _buildRow(context, item, index) {
     return Card(
-      child: ListTile(
-        title: Text(item.title),
-      subtitle: Text(item.subtitle),
-      onTap: () => item.push(context),
-        trailing: Icon(Icons.arrow_right),
-        leading: Icon(
-          item.icon,
-          size: 40,
+      margin: EdgeInsets.only(top: 20, left: 20, right: 20),
+      color: colors[index],
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListTile(
+          title: Text(
+            item.title,
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+          // subtitle: Text(item.subtitle),
+          onTap: () => item.push(context),
+          trailing: Icon(Icons.arrow_right),
+          leading: Icon(
+            item.icon,
+            size: 30,
+            color: Colors.white,
+          ),
         ),
       ),
     );
@@ -68,39 +84,63 @@ class _MyAppState extends State<MyApp> {
           appBar: AppBar(
             title: Text('VDO.Ninja'),
           ),
-          body: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(12, 30, 10, 15),
-                child: Container(
-                  width: double.infinity,
-                  child: Text(
-                    "Share",
-                    textAlign: TextAlign.left,
-                    style: theme.textTheme.headline1.apply(color: Colors.white),
+          body: Stack(children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 10, 15),
+                  child: Container(
+                    width: double.infinity,
+                    child: Text(
+                      "Share",
+                      textAlign: TextAlign.left,
+                      style: theme.textTheme.headline1.apply(
+                          color: Colors.white,
+                          fontWeightDelta: 10,
+                          fontSizeFactor: 1.5),
+                    ),
                   ),
                 ),
-              ),
-              ListView.builder(
-                  shrinkWrap: true,
-                  padding: const EdgeInsets.all(8.0),
-                  itemCount: items.length,
-                  itemBuilder: (context, i) {
-                    return _buildRow(context, items[i]);
-                  }),
-            ],
+                ListView.builder(
+                    shrinkWrap: true,
+                    padding: const EdgeInsets.all(8.0),
+                    itemCount: items.length,
+                    itemBuilder: (context, i) {
+                      return _buildRow(context, items[i], i);
+                    }),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 10.0),
+                      child: TextButton.icon(
+                          icon: Icon(Icons.help_center),
+                          style: Theme.of(context).elevatedButtonTheme.style,
+                          onPressed: () => {_openDiscord()},
+                          label: Text("Need help? Join our Discord.")),
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ]
           )),
     );
   }
 
   _initData() async {
     _prefs = await SharedPreferences.getInstance();
-	
-	setState(() {
-		Wakelock.enable();
-		// You could also use Wakelock.toggle(on: true);
-	});
-   
+
+    setState(() {
+      Wakelock.enable();
+      // You could also use Wakelock.toggle(on: true);
+    });
   }
 
   void showDemoDialog<T>({BuildContext context, Widget child}) {
@@ -114,22 +154,20 @@ class _MyAppState extends State<MyApp> {
           Navigator.push(
               context,
               MaterialPageRoute(
-                  builder: (BuildContext context) => CallSample(streamID: streamID, screenShare: _screenShare)));
+                  builder: (BuildContext context) => CallSample(
+                      streamID: streamID, screenShare: _screenShare)));
         }
       }
     });
   }
 
-	
-	
   _showAddressDialog(context) {
-	
     showDemoDialog<DialogDemoAction>(
         context: context,
         child: AlertDialog(
             title: const Text('Stream ID:'),
             content: TextField(
-               onChanged: (String text) {
+              onChanged: (String text) {
                 setState(() {
                   streamID = text;
                 });
@@ -154,45 +192,55 @@ class _MyAppState extends State<MyApp> {
   }
 
   _initItems() async {
-    
-	items = <RouteItem>[];
-	
-		items.add(
-			RouteItem(
-			  title: 'Screen',
-        subtitle: 'Share your complete screen',
+    items = <RouteItem>[];
+
+    items.add(RouteItem(
+        title: 'SCREEN',
+        subtitle: 'Share your device\'s screen',
         icon: Icons.screen_share,
-			  push: (BuildContext context) {
-				_screenShare="screen";
-				_showAddressDialog(context);
-			  }
-			)
-		);
-		
-		var devices = await navigator.mediaDevices.enumerateDevices();
-		for (var item in devices) {
-			if (item.kind != "videoinput"){continue;}
-			items.add(
-				RouteItem(
-				  title: item.label
-              .toString()
-              .replaceAllMapped(
-                  RegExp(r'Camera (\d), '), (Match m) => "Camera ${m[1]} - ")
-              .replaceAll(RegExp(r', Orientation \d{1,3}'), ""),
+        push: (BuildContext context) {
+          _screenShare = "screen";
+          _showAddressDialog(context);
+        }));
+
+    var devices = await navigator.mediaDevices.enumerateDevices();
+    for (var item in devices) {
+      if (item.kind != "videoinput") {
+        continue;
+      }
+
+      var cameraType = "Camera";
+
+      if (item.label.contains('back')) {
+        cameraType = 'Back Camera';
+      }
+
+      if (item.label.contains('front')) {
+        cameraType = 'Front Camera';
+      }
+
+      items.add(RouteItem(
+          title: cameraType.toUpperCase(),
           subtitle:
-              item.label.toString().replaceAll(RegExp(r'Camera \d, '), ''),
+              item.label.toString(),
           icon: item.label.contains('back')
               ? Icons.video_camera_back
               : Icons.video_camera_front,
-				  push: (BuildContext context) {
-					_screenShare=item.deviceId;
-					_showAddressDialog(context);
-				  }
-				)
-			);
-		}
-		
-		setState(() {});
-			
+          push: (BuildContext context) {
+            _screenShare = item.deviceId;
+            _showAddressDialog(context);
+          }));
+    }
+
+    setState(() {});
   }
+
+_openDiscord() async {
+    const url = 'https://discord.vdo.ninja';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+}
 }
